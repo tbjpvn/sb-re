ce47b84376c4dbe518514c0ff4552da89d0955bc
tbjpvn <tbjpvn@gmail.com>
2026-08-18 02:30:44 +0800

    Update sbre.sh

--- sbre.sh
+++ sbre.sh
@@ -867,7 +867,7 @@ uninstall_singbox() {
 create_shortcut() {
     cat > "$work_dir/sb.sh" << 'EOF'
 #!/usr/bin/env bash
-bash <(curl -Ls https://raw.githubusercontent.com/tbjpvn/sb-re/main/sbre.sh) $1
+bash <(curl -Ls https://raw.githubusercontent.com/tbjpvn/sb-re/main/sbre) $1
 EOF
     chmod +x "$work_dir/sb.sh"
     ln -sf "$work_dir/sb.sh" /usr/bin/sb
